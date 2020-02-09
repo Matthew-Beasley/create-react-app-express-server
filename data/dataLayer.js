3b7cc4bef0b02232f9f9f809bf14b901b418f2d6
conbec <56614504+conbec@users.noreply.github.com>
2020-02-09 13:51:55 -0800

    data layer complete

---
+++ data/dataLayer.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const writeJSON = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, JSON.stringify(data, null, 2), (err) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve();
+      }
+    })
+  })
+}
+
+
+const readJSON = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (data) {
+        try {
+          resolve(JSON.parse(data));
+        }
+        catch (ex) {
+          reject(ex);
+        }
+      }
+      else {
+        reject(err);
+      }
+    })
+  })
+}
